494a37446bceabaa1060acab94901194015af041
cao <cao@caodeMacBook-Air.local>
2024-05-26 22:15:21 +0800

    review code

--- config/config.js
+++ config/config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  nodeModulesTransform: {
+    type: "none",
+  },
+  fastRefresh: {},
+  dynamicImport: {},
   targets: {
     ie: 11,
   },
-  // layout: {
-  //   locale: true,
-  // },
   locale: {
     default: "en-US",
     antd: true,
     baseNavigator: false,
-  }
+  },
+  title: "LIVOLTEK",
+  favicon: "/favicon.png",
+  mountElementId: 'app',
 });
