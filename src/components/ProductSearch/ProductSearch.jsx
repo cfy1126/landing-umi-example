6af69e7e6e89cba0570509ef3e0d2458d0b68ea3
曹发友(80016865) <fayou.cao@hxgroup.com>
2024-10-14 10:27:32 +0800

    动态产品数据渲染在搜索页面

--- src/components/ProductSearch/ProductSearch.jsx
+++ src/components/ProductSearch/ProductSearch.jsx
@@ -1,12 +1,63 @@
 import React, { useState } from "react";
 import { Modal, Tabs, Input } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
-import styles from './ProductSearch.less';
+import { connect } from "umi";
+import styles from "./ProductSearch.less";
 const { Search } = Input;
 
-
-export default function ProductSearch() {
+function ProductSearch({ productCategory, productInfo }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data: categories } = productCategory || { data: [] };
+  const { data: products } = productInfo || { data: [] };
+
+  const CategoryContent = ({ item }) => {
+    return (
+      <>
+        <h3>{item.name}</h3>
+        {categories
+          .filter(
+            (_item) =>
+              _item.parent_code !== null &&
+              _item.parent_code.split(",").includes(item.code)
+          )
+          .map((element) => {
+            return (
+              <React.Fragment key={element.id}>
+                <div className={styles.second_title}>{element.name}</div>
+                <ul className={styles.product_list}>
+                  {products
+                    .filter(
+                      (product) =>
+                        product.category_system === item.code &&
+                        product.category_scene === element.code
+                    )
+                    .map((product) => {
+                      return (
+                        <li key={product.id}>
+                          <a href={`/productDetail?id=${product.code}`}>
+                            {product.name}
+                          </a>
+                        </li>
+                      );
+                    })}
+                </ul>
+              </React.Fragment>
+            );
+          })}
+      </>
+    );
+  };
+
+  const TabsItems = categories
+    .filter((item) => item.level === "1")
+    .map((item) => {
+      return (
+        <Tabs.TabPane tab={item.name} key={item.id}>
+          <CategoryContent item={item} />
+        </Tabs.TabPane>
+      );
+    });
+
   return (
     <>
       <MenuOutlined onClick={() => setIsModalOpen(true)} />
@@ -30,35 +81,22 @@ export default function ProductSearch() {
         <Search placeholder="input search text" enterButton />
         <Tabs tabPosition="left" style={{ flexGrow: 1 }}>
           <Tabs.TabPane tab="ALL" key="all">
-            <h3>RESIDENTIAL</h3>
-            <div
-              className={styles.second_title}
-            >
-              Grid-Tied
-            </div>
-            <ul className={styles.product_list}>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-                <li><a href="/productDetail?id=1">GT1-2K5/3K/3K3/3K6/4K/5K/6K-D2</a></li>
-            </ul>
+            {categories
+              .filter((object) => object.level === "1")
+              .map((item) => {
+                return <CategoryContent key={item.id} item={item} />;
+              })}
           </Tabs.TabPane>
-          <Tabs.TabPane tab="RESIDENTIAL" key="residential">
-            Content of Tab 1
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="COMMERCIAL & INDUSTRY" key="commercial_industry">
-            Content of Tab 2
-          </Tabs.TabPane>
+          {TabsItems}
         </Tabs>
       </Modal>
     </>
   );
 }
+
+export default connect(
+  ({ productInfo, productCategory }) => ({
+    productInfo,
+    productCategory,
+  })
+)(ProductSearch);
