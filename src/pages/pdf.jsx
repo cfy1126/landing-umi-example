511c53b7187557f38553739bd64ad27ef98ad8a0
曹发友(80016865) <fayou.cao@hxgroup.com>
2024-10-18 15:43:55 +0800

    使用react-pdf依赖渲染pdf

--- src/pages/pdf.jsx
+++ src/pages/pdf.jsx
@@ -1,48 +1,42 @@
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 import { useLocation } from "umi";
-import { useState, useEffect } from "react";
 import { Spin } from "antd";
+import "./pdf.less";
+pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.js`;
 
-const PdfViewer = () => {
+export default function PdfViewer() {
   const location = useLocation();
-  const { url } = location.state || {};
+  let { url } = location.state || {};
+  const newUrl = new URL(url);
+  let filePath = `/api${newUrl.pathname}`;
+  if (process.env.NODE_ENV === "production") {
+    filePath = url;
+  }
+
+  // PDF 页数相关的状态
+  const [numPages, setNumPages] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const iframe = document.getElementById("pdf-iframe");
-    const handleLoad = () => setLoading(false);
-
-    if (iframe) {
-      iframe.addEventListener("load", handleLoad);
-    }
-
-    return () => {
-      if (iframe) {
-        iframe.removeEventListener("load", handleLoad);
-      }
-    };
-  }, [url]);
+  // 处理 PDF 加载
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setLoading(false);
+  }
 
   return (
-    <>
-      {loading && (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-          <Spin size="large" />
-        </div>
-      )}
-      <iframe
-        id="pdf-iframe"
-        src={url}
-        style={{
-          position: "fixed",
-          top: 100,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          border: "none",
-        }}
-      />
-    </>
+    <div className="pdf-viewer-container">
+      <Spin spinning={loading}>
+        <Document
+          file={filePath}
+          onLoadSuccess={onDocumentLoadSuccess}
+          onLoadError={() => setLoading(false)}
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+          ))}
+        </Document>
+      </Spin>
+    </div>
   );
-};
-
-export default PdfViewer;
+}
